72ae3c3bd9a80b5a7ed27bf907c5a83752afb041
Yoshitaka <yoshitaka@ktgwya.jp>
2026-07-14 00:36:56 +0900

    add: item page

--- src/pages/item.ts
+++ src/pages/item.ts
@@ -2,9 +2,32 @@ import { join } from "jsr:@std/path";
 import { renderPage } from "../common/layout.ts";
 import { STATIC_DIR } from "../paths.ts";
 
+import itemsJson from "../../assets/data/item.json" with { type: "json" };
+
+type Item = {
+  name: string;
+  effect: string;
+  price: number;
+};
+
+const items = itemsJson as unknown as Item[];
+
 export async function handleItem(): Promise<Response> {
   const body = await Deno.readTextFile(
     join(STATIC_DIR, "pages/item.html"),
   );
-  return renderPage(body);
+
+  const itemHtml = items.map((item) => {
+    const effectCell = item.effect.replaceAll("、", "<br />");
+    const priceCell = item.price != null ? `${item.price.toLocaleString("ja-JP")}両` : "";
+
+    return `<tr><td>${item.name}</td><td>${effectCell}</td><td>${priceCell}</td></tr>`;
+  }).join("");
+
+  const html = `<table class="item-item">
+<thead><tr><th>商品名</th><th>効果</th><th>価格</th></tr></thead>
+<tbody>
+${body.replace("{{items}}", itemHtml)}</tbody></table>`;
+
+  return renderPage(html);
 }
--- src/pages/shop.ts
+++ src/pages/shop.ts
@@ -32,25 +32,33 @@ export async function handleShop(): Promise<Response> {
     join(STATIC_DIR, "pages/shop.html"),
   );
 
+  let html = "";
+
+  let navHtml = shops.map((country) => {
+    return `<li><a href="/shop#${country.country}">${country.country}</a></li>`;
+  }).join("");
+  navHtml = `<nav><ul>${navHtml}</ul></nav>`;
+
   const shopHtml = shops.map((country) => {
     const locations = country.location.map((location) => {
       const shopBlocks = location.shop.map((shop) => {
         const products = shop.product
           .map((product) =>
-            `<li><span class="shop-product-name">${product.name}</span><span class="shop-product-price">${product.price}両</span></li>`
+            `<tr><td><span class="shop-product-name">${product.name}</span></td><td><span class="shop-product-price">${product.price}両</span></td></tr>`
           )
           .join("");
 
-        return `<div class="shop-shop"><h4>${shop.shop}</h4><ul class="shop-product">${products}</ul></div>`;
+        return `<div class="shop-shop"><h4>${shop.shop}</h4><table class="shop-product"><thead><tr><th>商品名</th><th>価格</th></tr></thead><tbody>${products}</tbody></table></div>`;
       }).join("");
 
       return `<h3>${location.location}</h3>${shopBlocks}`;
     }).join("");
 
-    return `<div class="shop-country"><h2>${country.country}</h2><div class="shop-location">${locations}</div></div>`;
+    return `<div class="shop-country"><h2 id="${country.country}">${country.country}</h2><div class="shop-location">${locations}</div></div>`;
   }).join("");
 
-  const html = body.replace("{{shops}}", shopHtml);
+  html = body.replace("{{nav}}", navHtml);
+  html = html.replace("{{shops}}", shopHtml);
 
   return renderPage(html);
 }

9466fb4239007d2484c5fc96021be4a91f02f6d7
Yoshitaka <yoshitaka@ktgwya.jp>
2026-07-14 20:58:39 +0900

    add: hermit page

--- src/pages/hermit.ts
+++ src/pages/hermit.ts
@@ -2,9 +2,66 @@ import { join } from "jsr:@std/path";
 import { renderPage } from "../common/layout.ts";
 import { STATIC_DIR } from "../paths.ts";
 
+import hermitJson from "../../assets/data/hermit.json" with { type: "json" };
+
+type Hermit = {
+  name: string;
+  location: string;
+  scroll: string | null;
+  special: string | null;
+  note: string | null;
+};
+
+type Country = {
+  country: string;
+  hermit: Hermit[];
+};
+
+const hermits = hermitJson as unknown as Country[];
+
 export async function handleHermit(): Promise<Response> {
   const body = await Deno.readTextFile(
     join(STATIC_DIR, "pages/hermit.html"),
   );
-  return renderPage(body);
+
+  let navHtml = hermits.map((country) => {
+    return `<li><a href="/hermit#${country.country}">${country.country}</a></li>`;
+  }).join("");
+  navHtml = `<nav><ul>${navHtml}</ul></nav>`;
+
+  const countryHtml = hermits.map((country) => {
+    const hermitHtml = country.hermit.map((hermit) => {
+      const scroll = hermit.scroll != null ? hermit.scroll : "--";
+      const special = hermit.special != null ? hermit.special : "--";
+      const note = hermit.note != null ? hermit.note.replaceAll("、", "<br />") : "--";
+
+      return `<tr>
+        <td>${hermit.name}</td>
+        <td>${hermit.location}</td>
+        <td>${scroll}</td>
+        <td>${special}</td>
+        <td>${note}</td>
+      </tr>`;
+    }).join("");
+
+    return `<h4 id="${country.country}">${country.country}</h4>
+    <table class="hermit-hermit">
+    <thead>
+    <tr>
+    <th>仙人</th>
+    <th>場所</th>
+    <th>巻物</th>
+    <th>奥義</th>
+    <th>備考</th>
+  </tr>
+  </thead>
+  <tbody>${hermitHtml}</tbody>
+  </table>
+  </h4>`;
+  }).join("");
+
+  let html = body.replace("{{nav}}", navHtml);
+  html = html.replace("{{hermits}}", countryHtml);
+
+  return renderPage(html);
 }

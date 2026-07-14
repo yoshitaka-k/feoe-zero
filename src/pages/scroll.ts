05ebb44e95f1d641cef0f6e0ce6a058d61d8ae2e
Yoshitaka <yoshitaka@ktgwya.jp>
2026-07-14 19:36:22 +0900

    add: scroll page

--- src/pages/scroll.ts
+++ src/pages/scroll.ts
@@ -18,5 +18,29 @@ export async function handleScroll(): Promise<Response> {
   const body = await Deno.readTextFile(
     join(STATIC_DIR, "pages/scroll.html"),
   );
-  return renderPage(body);
+
+  const scrollHtml = scrolls.map((scroll) => {
+    return `<tr>
+<td class="scroll-name no-wrap">${scroll.name}</td>
+<td>${scroll.effect}</td>
+<td>${scroll.magic_point}</td>
+<td>${scroll.target}</td>
+<td>${scroll.location}</td>
+</tr>`;
+  }).join("");
+
+  return renderPage(`<table class="scroll-scroll">
+<thead>
+  <tr>
+    <th>巻物</th>
+    <th>効果</th>
+    <th>消費技</th>
+    <th>対象</th>
+    <th>場所</th>
+  </tr>
+</thead>
+<tbody>
+  ${body.replace("{{scrolls}}", scrollHtml)}
+</tbody>
+</table>`);
 }

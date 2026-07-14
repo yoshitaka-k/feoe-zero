b02eed25ed6c5b517b29ab725bc451f102ffd184
Yoshitaka <yoshitaka@ktgwya.jp>
2026-07-14 20:39:43 +0900

    add: special page

--- src/pages/special.ts
+++ src/pages/special.ts
@@ -2,9 +2,64 @@ import { join } from "jsr:@std/path";
 import { renderPage } from "../common/layout.ts";
 import { STATIC_DIR } from "../paths.ts";
 
+import specialJson from "../../assets/data/special.json" with { type: "json" };
+
+type Special = {
+  character: string;
+  name: string;
+  effect: string;
+  magic_point: string;
+  note: string;
+};
+
+type Character = {
+  character: string;
+  special: Special[];
+};
+
+const specials = specialJson as unknown as Character[];
+
 export async function handleSpecial(): Promise<Response> {
   const body = await Deno.readTextFile(
     join(STATIC_DIR, "pages/special.html"),
   );
-  return renderPage(body);
+
+  let navHtml = specials.map((character) => {
+    return `<li><a href="/special#${character.character}">${character.character}</a></li>`;
+  }).join("");
+  navHtml = `<nav><ul>${navHtml}</ul></nav>`;
+
+  const characterHtml = specials.map((character) => {
+    const specialHtml = character.special
+      .map((special) => {
+        const magicPoint = special.magic_point != null ? special.magic_point : "";
+
+        return `<tr>
+  <td class="special-name no-wrap">${special.name}</td>
+  <td>${special.effect}</td>
+  <td>${magicPoint}</td>
+  <td>${special.note}</td>
+</tr>`;
+}).join("");
+
+    return `<div class="special-character">
+<h4 id="${character.character}">${character.character}</h4>
+<table class="special-special">
+<thead>
+  <tr>
+    <th>奥義名</th>
+    <th>効果</th>
+    <th>消費技</th>
+    <th>条件</th>
+  </tr>
+</thead>
+<tbody>${specialHtml}</tbody>
+</table>
+</div>`;
+  }).join("");
+
+  let html = body.replace("{{specials}}", characterHtml);
+  html = html.replace("{{nav}}", navHtml);
+
+  return renderPage(html);
 }

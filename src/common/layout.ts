dc7641e34b23e2e26ecabcecd37043c8930947bc
Yoshitaka <yoshitaka@ktgwya.jp>
2026-07-16 17:34:03 +0900

    add: google tags

--- src/common/layout.ts
+++ src/common/layout.ts
@@ -14,8 +14,20 @@ export async function renderPage(body: string): Promise<Response> {
 <html lang="ja">
 <head>
 ${applyBasePath(head)}
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-WFR8C7XMQ9');
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P2Q83FT7');
+</script>
 </head>
 <body>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2Q83FT7" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 ${applyBasePath(header)}
 ${body}
 ${applyBasePath(footer)}
